b5df3cefacbd9fcbdc1169351c5672682e58810c
Tadeusz Miszczyk <42252259+8tm@users.noreply.github.com>
2024-05-12 01:20:28 +0200

    Fix Elvish and VM issues

--- src/device_drama/plugins/05_applications/01_shell_info.py
+++ src/device_drama/plugins/05_applications/01_shell_info.py
@@ -13,17 +13,23 @@ class DDPlugin(BasePlugin):
         self.info.author = 'Tadeusz Miszczyk'
         self.info.description = 'Return Shell info'
         self.info.name = 'Shell-info'
-        self.info.plugin_version = '23.9.16'
+        self.info.plugin_version = '24.5.13'
         self.info.compatible_version = '23.9.16'
 
     @staticmethod
     def get_shell_info() -> str:
-        shell_path = os.environ.get('SHELL', 'Unknown Shell')
-        shell_version_output = run_command(f'{shell_path} --version').output
-        shell_version = 'Unknown Version'
-        if shell_version_output.split():
-            shell_version = shell_version_output.split()[3].split('(')[0]
-        return f'{os.path.basename(shell_path).title()} {shell_version}'
+        shell_path = os.environ.get('SHELL', None)
+        shell_info = '<Unknown>'
+
+        if shell_path is not None:
+            shell_version_output = run_command(f'{shell_path} --version').output
+            shell_version = '<Unknown Version>'
+            if 'elvish' in shell_path:
+                shell_version = shell_version_output
+            elif shell_version_output.split():
+                shell_version = shell_version_output.split()[3].split('(')[0]
+            shell_info = f'{os.path.basename(shell_path).title()} {shell_version}'
+        return shell_info
 
     def run(self) -> List[Dict[str, str]]:
         return [
