4e463021f9e84fbd9630fe893c04b223958f4478
Tadeusz Miszczyk <42252259+8tm@users.noreply.github.com>
2024-05-12 01:05:46 +0200

    Fix Elvish and VM issues

--- src/device_drama/plugins/01_main_hardware/01_mobo_info.py
+++ src/device_drama/plugins/01_main_hardware/01_mobo_info.py
@@ -1,4 +1,5 @@
 from typing import Dict, List
+from pathlib import Path
 
 from device_drama.classes.base_plugin import BasePlugin  # type: ignore
 from device_drama.classes.logger import Logger  # type: ignore
@@ -11,15 +12,19 @@ class DDPlugin(BasePlugin):
         self.info.author = 'Tadeusz Miszczyk'
         self.info.description = 'Return MoBo info'
         self.info.name = 'MoBo-Info'
-        self.info.plugin_version = '23.9.12'
+        self.info.plugin_version = '24.5.12'
         self.info.compatible_version = '23.9.12'
 
     def run(self) -> List[Dict[str, str]]:
-        with open('/sys/devices/virtual/dmi/id/sys_vendor', 'r') as mobo_company_file:
-            company = mobo_company_file.read().strip()
+        company = '<Unknown>'
+        if Path('/sys/devices/virtual/dmi/id/sys_vendor').exists():
+            with open('/sys/devices/virtual/dmi/id/sys_vendor', 'r') as mobo_company_file:
+                company = mobo_company_file.read().strip()
 
-        with open('/sys/devices/virtual/dmi/id/board_name', 'r') as mobo_name_file:
-            model = mobo_name_file.read().strip()
+        model = '<Unknown>'
+        if Path('/sys/devices/virtual/dmi/id/board_name').exists():
+            with open('/sys/devices/virtual/dmi/id/board_name', 'r') as mobo_name_file:
+                model = mobo_name_file.read().strip()
 
         return [
             {
